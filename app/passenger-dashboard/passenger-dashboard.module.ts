5c8428fb7e2422089e5deda4dfafbc74c57995f5
Jonathan Grupp <jonathan.grupp@nolteundlauth.de>
2017-06-17 16:21:26 +0200

    presentational components - passenger-detail, passenger-count - https://courses.ultimateangular.com/courses/100820/lectures/1944326

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
+import { PassengerCountComponent } from "./components/passenger-count/passenger-count.component";
+import { PassengerDetailComponent } from "./components/passenger-detail/passenger-detail.component";
+
 
 @NgModule({ // configuration object for NgModule decorator
   declarations: [
     PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent,
   ],
   imports: [
     CommonModule,
