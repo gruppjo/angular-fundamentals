7cb0f3b952765c9b2c48dd056a1d182c5c6218d7
Jonathan Grupp <jonathan.grupp@nolteundlauth.de>
2017-06-17 16:42:48 +0200

    @Input - https://courses.ultimateangular.com/courses/100820/lectures/1944346

--- app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -1,14 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+
+import { Passenger } from "../../models/passenger.interface";
 
 @Component({
   selector: 'passenger-count',
 
   template: `
     <div>
-      Count Component
+      <h3> Airline Passenger </h3>
+      <div>
+        Total passengers: {{ checkedInCount() }} / {{ items.length }}
+      </div>
     </div>
   `
 })
 export class PassengerCountComponent {
+  @Input() items: Passenger[];
   constructor () {}
+  checkedInCount(): number {
+    if (!this.items) return;
+    return this.items.filter((passenger: Passenger) => passenger.checkedIn).length;
+  }
 }
