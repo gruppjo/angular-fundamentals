966cf964742c7404b8dbf1654c8d20ac5a137e7d
Jonathan Grupp <jonathan.grupp@nolteundlauth.de>
2017-06-17 16:04:50 +0200

    off-road: move passenger-dashboard template to own file

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -5,26 +5,7 @@ import { Passenger } from "../../models/passenger.interface";
 @Component({
   selector: 'passenger-dashboard',
   styleUrls: ['passenger-dashboard.component.scss'],
-  template: `
-    <div>
-      <h3> Airline Passenger </h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span
-            class="status"
-            [class.checked-in]="passenger.checkedIn"></span>
-          {{ i }}: {{ passenger.fullname }}
-          <div class="date">
-            Check in date:
-            {{ passenger.checkInDate ? (passenger.checkInDate | date | uppercase) : 'not checked in'}}
-          </div>
-          <div class="children">
-            Children: {{ passenger.children?.length || 0 }}
-          </div>
-        </li>
-      </ul>
-    </div>
-  `
+  templateUrl: 'passenger-dashboard.component.html'
 })
 export class PassengerDashboardComponent {
   passengers: Passenger[] = [{
